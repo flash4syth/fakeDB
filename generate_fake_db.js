86ee4cf46f002672f867f01372700088b667f63f
Jon Forsyth <jon4syth@gmail.com>
2015-10-10 15:43:03 -0600

    change to loop 10 times, add email field

--- generate_fake_db.js
+++ generate_fake_db.js
@@ -7,10 +7,11 @@ var faker = require('faker');
 
 var results = [];
 
-for(var i = 10; i < 11; i += 1){
+for(var i = 0; i <= 10; i += 1){
 	var obj = {};
 	obj.student_record = {}
 	obj.student_record.ticket = faker.random.uuid().substring(0,8);
+	obj.student_record.email = faker.internet.email();
 	results.push(obj);
 }
 console.log(JSON.stringify(results, null, 2));
